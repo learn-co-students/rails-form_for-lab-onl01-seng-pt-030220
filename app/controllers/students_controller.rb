4777d638cb288debc8fa88e9e74b46ab3a6f25e6
jordles113 <jordan.t.romero@gmail.com>
2020-07-04 15:01:24 -0500

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,48 @@
+class StudentsController < ApplicationController
+
+    def index 
+        @students = Student.all 
+    end 
+    
+    def new 
+        @student = Student.new
+    end 
+    
+    def show 
+        @student = Student.find(params[:id]) 
+    end 
+
+    def create 
+        @student = Student.new(student_params)
+        @student.save 
+        redirect_to student_path(@student)
+    end 
+
+    def edit 
+        @student = Student.find(params[:id])
+    end 
+
+    def update
+        @student = Student.find(params[:id])
+        @student.update(student_params)
+        @student.save
+        redirect_to student_path(@student)
+    end 
+
+    def destroy
+        @student = Student.find(params[:id])
+        @student.destroy
+        redirect_to students_path 
+    end 
+
+
+
+
+
+
+    private 
+
+    def student_params 
+        params.require(:student).permit(:first_name, :last_name)
+    end 
+end
