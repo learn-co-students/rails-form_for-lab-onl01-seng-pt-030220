8bd89c04acc33004f15c76ca94dfaf49a5b7ebe9
cdominick091984 <cdominick091984@gmail.com>
2020-07-11 21:48:57 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,9 @@ Rails.application.routes.draw do
   resources :students, only: [:new, :create, :edit, :update, :show]
   resources :school_classes, only: [:new, :create, :edit, :update, :show]
 end
+
+# could have done it like this:
+# Rails.application.routes.draw do
+#   resources :students, except: [:destroy, :index]
+#   resources :school_classes, except: [:destroy, :index]
+# end
